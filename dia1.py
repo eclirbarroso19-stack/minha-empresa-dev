a297fe8cbb1c640e8f9dc067a8263ed07ce5adad
Eclir Barroso <eclirbarroso19@gmail.com>
2026-09-05 19:11:53 -0300

    Dia 2 - empresa com nome

--- dia1.py
+++ dia1.py
@@ -1,1 +1,8 @@
 print('Dia 1 - Início da minha empresa')
+print('Dia 2 - Minha empresa dev')
+
+nome_empresa = input('Qual o nome da sua empresa? ')
+dono = input('Qual seu nome? ')
+
+print(f'\nSeja bem-vinda, {nome_empresa}!')
+print(f'Fundada por {dono} - rumo aos 30 dias!')
